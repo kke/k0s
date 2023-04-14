ed4dba731067cd929befb63ca3eb9196900149d2
Kimmo Lehto <klehto@mirantis.com>
2023-04-14 08:20:54 +0300

    Beautify status/client

--- pkg/component/status/client.go
+++ pkg/component/status/client.go
@@ -55,7 +55,7 @@ type ProbeStatus struct {
 // GetStatus returns the status of the k0s process using the status socket
 func GetStatusInfo(socketPath string) (*K0sStatus, error) {
 	status := &K0sStatus{}
-	if err := doHTTPRequestViaUnixSocket(socketPath, "status", status); err != nil {
+	if err := statusSocketRequest(socketPath, "status", status); err != nil {
 		return nil, err
 	}
 	return status, nil
@@ -64,7 +64,7 @@ func GetStatusInfo(socketPath string) (*K0sStatus, error) {
 // GetComponentStatus returns the per-component events and health-checks
 func GetComponentStatus(socketPath string, maxCount int) (*prober.State, error) {
 	status := &prober.State{}
-	if err := doHTTPRequestViaUnixSocket(socketPath,
+	if err := statusSocketRequest(socketPath,
 		fmt.Sprintf("components?maxCount=%d", maxCount),
 		status); err != nil {
 		return nil, err
@@ -72,9 +72,10 @@ func GetComponentStatus(socketPath string, maxCount int) (*prober.State, error) 
 	return status, nil
 }
 
-func doHTTPRequestViaUnixSocket(socketPath string, path string, tgt interface{}) error {
+func statusSocketRequest(socketPath string, path string, tgt interface{}) error {
 	httpc := http.Client{
 		Transport: &http.Transport{
+			// todo: pass context
 			DialContext: func(_ context.Context, _, _ string) (net.Conn, error) {
 				return net.Dial("unix", socketPath)
 			},
@@ -95,8 +96,9 @@ func doHTTPRequestViaUnixSocket(socketPath string, path string, tgt interface{})
 		return fmt.Errorf("status: unexpected http status code: %v %v", socketPath, path)
 	}
 
-	if err := json.NewDecoder(response.Body).Decode(tgt); err != nil {
-		return fmt.Errorf("status: can't decode json: %v", err)
+	decoder := json.NewDecoder(response.Body)
+	if err := decoder.Decode(tgt); err != nil {
+		return fmt.Errorf("status: can't decode json: %w", err)
 	}
 	return nil
 }
